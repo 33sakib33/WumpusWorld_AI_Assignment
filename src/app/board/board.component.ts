eabff9b30ff6eace255d7931b84eb6b899d72e7c
33sakib33 <bsse1108@iit.du.ac.bd>
2022-10-11 04:33:49 +0600

    walls

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { Board } from './board';
 
 @Component({
   selector: 'app-board',
@@ -7,12 +8,32 @@ import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit } from '
 })
 export class BoardComponent implements OnInit, AfterViewInit {
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef) {
+    
+		for (var i = 0; i < 10; i++) {
+			this.boardMatrix[i] = Array(10).fill({"wall":0,"stuff": 0});
+		}
+   }
+  board : Board = new Board(10);
+  boardMatrix : any[][] = []; 
+  iter: any = Array(100);
 
   ngOnInit(): void {
+
   }
   ngAfterViewInit(): void {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor="#EEA47F";
   }
+  getWall(idx: number) : number{
+    console.log(idx);
+    let xPos:any=Math.floor(idx/10);
+    let yPos:any =idx%10;
+    console.log(xPos);
+    console.log(yPos);
+    let retValue=this.boardMatrix[xPos][yPos].wall;
+    console.log(retValue);
+    console.log("hehe");
+    return retValue;
 
+  }
 }
